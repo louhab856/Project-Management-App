512909ceebc3ce7c61ba55c2d4e308dea62d4479
louhab abderazzak <louhab.abderazzak.pro@gmail.com>
2024-07-04 14:53:38 +0200

    the second commit

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,8 +1,41 @@
 import Pagination from '@/Components/Pagination';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head, Link } from '@inertiajs/react';
+import { Head, Link, router } from '@inertiajs/react';
+import {PROJECT_STATUS_TEXT_MAP ,PROJECT_STATUS_CLASS_MAP} from '../../constante.jsx'
+import TextInput from '@/Components/TextInput';
+import SelectInpute from '@/Components/SelectInpute.jsx';
+import { ChevronUpIcon,ChevronDownIcon } from '@heroicons/react/16/solid'
 
-export default function index({ auth , projects}){
+
+export default function index({ auth , projects , queryParams = null }){
+    queryParams = queryParams || {};
+    const searchFieldChanged = (name, value) => {
+      if (value) {
+        queryParams[name] = value;
+      } else {
+        delete queryParams[name];
+      }
+  
+      router.get(route("project.index"), queryParams);
+    };
+    const onKeyPress = (name , e)=>{
+        if(e.key != 'Enter') return ;
+        searchFieldChanged(name,e.target.value)
+    }
+    const sortChanged = (name)=>{
+        if(name === queryParams.sort_field){
+            if(queryParams.sort_field === 'asc'){
+                queryParams.sort_direction = 'desc'
+            }
+            else {
+                queryParams.sort_direction = 'asc'
+            }
+        }else {
+            queryParams.sort_field = name ;
+            queryParams.sort_direction = 'asc';
+        }
+        router.get(route("project.index"), queryParams);
+    }
     return (
     <AuthenticatedLayout
     user={auth.user}
@@ -18,29 +51,77 @@ export default function index({ auth , projects}){
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div className="p-6 text-gray-900 dark:text-gray-100">
-                    <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
+                    <div className='overflow-auto'>
+                    <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>  
                         <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gay-500'>
                             <tr>
-                                <th className='px-3 py-2'> Id </th>
-                                <th className='px-3 py-2'> Image </th>  
-                                <th className='px-3 py-2' > Name </th>  
-                                <th className='px-3 py-2'> Staus </th>  
-                                <th className='px-3 py-2'> Created date </th>  
-                                <th className='px-3 py-2'> Due date</th>  
-                                <th className='px-3 py-2'> Create by</th>  
-                                <th className='px-3 py-2'> Actions </th>  
+                                <th className='px-3 py-2' >
+
+                                </th>
+                                <th className='px-3 py-2'>
+                                
+                                </th>
+                                <th className='px-3 py-2'>
+                                <TextInput 
+                                defaultValue ={queryParams.name}
+                                className="w-full" 
+                                plcholder="Project Name" 
+                                onBlur={e=> searchFieldChanged('name' , e.target.value)}
+                                onKeyPress= {e => onKeyPress('name' , e.target.value)} 
+                                />
+                                </th>
+                                <th className='px-3 py-2' > 
+                                    <SelectInpute 
+                                    onChange={ e => searchFieldChanged('status' , e.target.value)}
+                                    defaultValue ={queryParams.name}
+                                    >
+                                        <option value=''>Select staus</option>
+                                        <option value='pending'>Pending</option>
+                                        <option value='in_progress'>In Progress</option>
+                                        <option value='completed'>Completed</option>
+                                    </SelectInpute>
+                                </th>
+                                <th className='px-3 py-2'> 
+                                     </th>  
+                                <th className='px-3 py-2'> 
+                                </th>  
+                                <th className='px-3 py-2'> 
+                                    
+                                </th>  
+                                <th className='px-3 py-2'> 
+                            
+                             </th>  
                             </tr>
-                        </thead>
+                        </thead> 
+                        <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gay-500'>
+                            <tr>
+                                <th  onClick = {()=> sortChanged('id')} className='px-3 py-2 flex flex-items-center justify-between gap-1'  >
+                                Id  <ChevronUpIcon className='w-4' />< ChevronDownIcon  className='w-4 -mt-2' /> 
+                                </th>
+                                    
+                                <th  className='px-3 py-2'>Image</th>
+                                <th onClick={() => sortChanged('name')} className='px-3 py-2'>name</th>
+                                <th onClick = {()=> sortChanged('status')} className='px-3 py-2' > status</th>
+                                <th onClick = {()=> sortChanged('created_at')} className='px-3 py-2'> Created date </th>  
+                                <th onClick = {()=> sortChanged('due_date')} className='px-3 py-2'> Due date</th>  
+                                <th className='px-3 py-2'> Create by</th>  
+                                <th  className='px-3 py-2'> Actions </th>  
+                            </tr>
+                         </thead>
                         <tbody>
                             {
                                projects.data.map((project)=>(
-                                <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'>
+                                <tr  key={project.id}  className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'>
                                     <th className='px-3 py-2'> {project.id}</th>
                                     <td className='px-3 py-2'> 
                                         <img style={{width:60}} src={project.image_path} alt="" />
                                     </td>
                                     <td className='px-3 py-2'> {project.name}</td>
-                                    <td className='px-3 py-2'> {project.status}</td>    
+                                    <td className='px-3 py-2'> 
+                                    <span className={`px-2 py-1 rounded text-white ${PROJECT_STATUS_CLASS_MAP[project.status]}`}>
+                                                    {PROJECT_STATUS_TEXT_MAP[project.status]}
+                                    </span>                                           
+                                    </td>    
                                     <td className='px-3 py-2'> {project.created_at}</td>
                                     <td className='px-3 py-2'> {project.due_date}</td>
                                     <td className='px-3 py-2'> {project.createdBy.name}</td>
@@ -67,6 +148,7 @@ export default function index({ auth , projects}){
                             }
                         </tbody>
                     </table>
+                    </div>
                     <Pagination links={projects.meta.links}/>
                 </div>
             </div>
